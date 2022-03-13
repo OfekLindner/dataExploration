330555552c248afa63b1a18dc4737ed2189aa401
ofirasulin3 <ofirasulin3@gmail.com>
2022-03-13 15:21:37 +0200

    adding wigle.py

---
+++ wigle.py
@@ -0,0 +1,22 @@
+import json
+import requests
+from pygle import network
+from requests.auth import HTTPBasicAuth
+
+# url for config file: C:\Users\ofira\Anaconda3\Lib\site-packages\pygle\config.py
+def check_if_BSSID_exists(BSSID):
+    return network.detail(netid=BSSID)
+
+def check_if_BSSID_exists_REST(BSSID):
+    report_url = 'https://api.wigle.net/api/v2/network/detail'
+    # params = {'Authorization':'Basic QUlEN2I0MWM5MWY4MmVkODc4MmIyZjM2YTU3OTk2MzcyMTU6NmRhZTZlOWUwNWJjNDRhOTFmODJlZjRlMTA5ODkzZjE=',
+    #           'netid': BSSID}
+    params = {'netid': BSSID}
+    # sending request to WiGLE API and get a response:
+    response = requests.get(report_url, params=params)
+
+    if response.status_code == 401:
+        response = requests.get('https://api.wigle.net/api/v2/network/detail',
+                                auth=HTTPBasicAuth('networisk', 'ofirofek123'))
+
+    return response.status_code
